dafd5629a279939e220d7638d6807b987c076908
Reid Isaki <risaki@revision3.com>
2014-09-15 16:20:28 -0700

    updated voice recognition prompt

--- src/com/yoneko/areyouthereyet/update/MapActivity.java
+++ src/com/yoneko/areyouthereyet/update/MapActivity.java
@@ -1120,7 +1120,7 @@ OnAddGeofencesResultListener, LocationListener, OnRemoveGeofencesResultListener,
 		Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
 		intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,
 				RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
-		intent.putExtra(RecognizerIntent.EXTRA_PROMPT, "Voice recognition Demo...");
+		intent.putExtra(RecognizerIntent.EXTRA_PROMPT, "Speak now...");
 		startActivityForResult(intent, REQUEST_CODE);
 	}
 
