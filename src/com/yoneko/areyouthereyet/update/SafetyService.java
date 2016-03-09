34ee6dfbba95934d2bd043f0f362f013e5225ac7
Reid Isaki <reidisaki@yahoo.com>
2016-03-09 08:00:31 -0800

    testing intent service with no kill boot up

--- src/com/yoneko/areyouthereyet/update/SafetyService.java
+++ src/com/yoneko/areyouthereyet/update/SafetyService.java
@@ -1,15 +1,33 @@
 package com.yoneko.areyouthereyet.update;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import android.app.PendingIntent;
 import android.app.Service;
 import android.content.Intent;
 import android.os.Binder;
 import android.os.Bundle;
 import android.os.IBinder;
 import android.util.Log;
+import android.widget.Toast;
 
-import com.flurry.android.FlurryAgent;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.GoogleApiClient.ConnectionCallbacks;
+import com.google.android.gms.common.api.GoogleApiClient.OnConnectionFailedListener;
+import com.google.android.gms.common.api.PendingResult;
+import com.google.android.gms.common.api.ResultCallback;
+import com.google.android.gms.common.api.Status;
+import com.google.android.gms.location.Geofence;
+import com.google.android.gms.location.GeofencingRequest;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
+import com.yoneko.models.SimpleGeofence;
 
-public class SafetyService extends Service {
+public class SafetyService extends Service implements ConnectionCallbacks, OnConnectionFailedListener {
+	LocationRequest mLocationRequest;
+	GoogleApiClient mGoogleApiClient;
 	public SafetyService() {
 	}
 
@@ -21,6 +39,8 @@ public class SafetyService extends Service {
 	}
 
 	private final IBinder mBinder = new LocalBinder();
+	private PendingIntent mTransitionPendingIntent;
+	private List<SimpleGeofence> mSimpleGeoFenceList;
 
     public class LocalBinder extends Binder {
     	SafetyService getService() {
@@ -30,18 +50,21 @@ public class SafetyService extends Service {
 	@Override
 	public int onStartCommand(Intent intent, int flags, int startId) {
 		Log.i("Reid1","OnStartCommand");
-//		//Toast.makeText(getApplicationContext(), "Areyouthereyet coming into play", Toast.LENGTH_LONG).show();
-		if(!MapActivity.isActive) {
-			FlurryAgent.logEvent("Restarted the app for some reason..");
-			Intent i = new Intent(getApplicationContext(), MapActivity.class);
-			i.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-			Bundle b = new Bundle();
-			b.putBoolean("moveToBack", true);
-			i.putExtras(b);
-//			startActivity(i);			
-		}
+
+		//do code in here
+		init();
 		return Service.START_STICKY;
 	}
+	private void init() {
+		mLocationRequest = LocationRequest.create();
+		mGoogleApiClient = new GoogleApiClient.Builder(this)
+		.addApi(LocationServices.API).addConnectionCallbacks(this)
+		.addOnConnectionFailedListener(this).build();
+
+		
+		
+	}
+
 	@Override
 	public void onDestroy() {
 		Log.i("Reid1","Killing safety service");
@@ -50,4 +73,107 @@ public class SafetyService extends Service {
 		super.onDestroy();
 		
 	}
+	public void removeGeofences( PendingIntent requestIntent) {
+		PendingResult<Status> result = LocationServices.GeofencingApi.removeGeofences(mGoogleApiClient,
+				requestIntent);
+		result.setResultCallback(new ResultCallback<Status>() {
+			@Override
+			public void onResult(Status result) {
+				if(result.isSuccess()) {
+						addGeofences();
+					Log.i("Reid","removed fences successfully");
+				} else {
+					Log.i("Reid","removed fences UNsuccessfully");
+				}
+
+			}
+		});
+	}
+	public void addGeofences(){
+		ArrayList<Geofence> geoFences = new ArrayList<Geofence>();
+		mTransitionPendingIntent = getTransitionPendingIntent();
+		// for(int i=0; i<mSimpleGeoFenceList.size();i++) {
+		// add items to geoFences;
+		try {
+			
+				for (SimpleGeofence g : mSimpleGeoFenceList) {
+					Log.i("Reid",
+							"geofence number registerd:"
+									+ g.getEmailPhone() + " name: " + g.getId() +  " title: " + g.getTitle());
+					geoFences.add(g.toGeofence());
+				}
+				//TODO: check if this is valid, we want to restart geo fences frequently since they get dropped serverside frequently
+//				reRegisterGeoFences = false;
+			
+		} catch (IllegalArgumentException e) {
+			
+		}
+		//			mInProgress = true;
+		if(geoFences.size() > 0) {
+			GeofencingRequest fenceRequest = new GeofencingRequest.Builder().addGeofences(geoFences).build();
+			PendingResult<Status> result = LocationServices.GeofencingApi.addGeofences(mGoogleApiClient,fenceRequest,mTransitionPendingIntent);
+
+			result.setResultCallback(new ResultCallback<Status>() {
+				@Override
+				public void onResult(Status result) {
+					if(result.isSuccess()) {
+						//TODO: this isnt really doing anything.
+						Log.i("Reid","added fences successfully");
+						Toast.makeText(getApplicationContext(), "SUCCESS, added fence",Toast.LENGTH_SHORT).show();
+					} else {
+						//						TODO:
+						//there was an error relay this to the user..
+						Toast.makeText(getApplicationContext(), "ERROR, please try again:" + result.getStatusMessage(),Toast.LENGTH_SHORT).show();
+					}
+
+				}
+			});
+		}
+	}
+
+	
+		private PendingIntent getTransitionPendingIntent() {
+
+			// Create an Intent pointing to the IntentService
+
+			// Intent intent = new Intent(context,
+			// ReceiveTransitionsIntentService.class);
+			/*
+			 * Return a PendingIntent to start the IntentService. Always create a
+			 * PendingIntent sent to Location Services with FLAG_UPDATE_CURRENT, so
+			 * that sending the PendingIntent again updates the original. Otherwise,
+			 * Location Services can't match the PendingIntent to requests made with
+			 * it.
+			 */
+
+			// If the PendingIntent already exists
+			if (null != mTransitionPendingIntent) {
+
+				// Return the existing intent
+				return mTransitionPendingIntent;
+
+				// If no PendingIntent exists
+			} else {
+				Intent intent = new Intent(
+						"com.yoneko.areyouthereyet.ACTION_RECEIVE_GEOFENCE");
+				return PendingIntent.getBroadcast(this, 0, intent,
+						PendingIntent.FLAG_UPDATE_CURRENT);
+			}
+	}
+
+		@Override
+		public void onConnectionFailed(ConnectionResult result) {
+			
+		}
+
+		@Override
+		public void onConnected(Bundle connectionHint) {
+			removeGeofences(getTransitionPendingIntent());
+			
+		}
+
+		@Override
+		public void onConnectionSuspended(int cause) {
+			
+		}
 }
